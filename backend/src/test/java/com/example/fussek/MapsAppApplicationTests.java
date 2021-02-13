9ede5b4b11886198e1efc544ecdd13044708d9bc
patryk.fussek.1996 <patryk.fussek@campus.tu-berlin.de>
2021-02-13 19:57:27 +0100

    Initial commit: Java & Spring Backend

---
+++ backend/src/test/java/com/example/fussek/MapsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package java.com.example.fussek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
